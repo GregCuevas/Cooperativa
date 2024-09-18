ba614fa268bbb8be814040eba244d15917ed8795
GregCuevas <gregorycuevas12@icloud.com>
2024-09-18 11:38:24 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,310 +1,183 @@
-import React, { useState, useEffect, useRef } from "react";
-import Logo from "../assets/logo.png";
-import { X, Menu, Search, ChevronDown } from "lucide-react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu, X, ChevronDown, Search } from "lucide-react";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [activeDropdown, setActiveDropdown] = useState(null); // Manejar el dropdown activo
-  const dropdownRef = useRef(null);
+  const [activeDropdown, setActiveDropdown] = useState("");
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const toggleDropdown = (menu) => {
+    setActiveDropdown(activeDropdown === menu ? "" : menu);
   };
 
-  const toggleDropdown = (key) => {
-    if (activeDropdown === key) {
-      setActiveDropdown(null); // Cierra si ya está abierto
-    } else {
-      setActiveDropdown(key); // Abre el nuevo dropdown
-    }
-  };
+  const topMenuItems = [
+    { name: "HAZTE SOCIO", link: "/hazte-socio" },
+    { name: "SUCURSALES", link: "/sucursales" },
+    { name: "CONTÁCTANOS", link: "/contactanos" },
+    { name: "PREGUNTAS FRECUENTES", link: "/preguntas-frecuentes" },
+    { name: "APP MÓVIL", link: "/app-movil" },
+  ];
 
-  // Manejar el clic fuera del menú
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setActiveDropdown(null); // Cerrar dropdown si se hace clic fuera
-      }
-    };
+  const bottomMenuItems = [
+    {
+      name: "PRODUCTOS Y SERVICIOS",
+      link: "/productos-y-servicios",
+      hasSubmenu: true,
+    },
+    { name: "CONÓCENOS", link: "/conocenos", hasSubmenu: true },
+    { name: "DESEMPEÑO SOCIAL", link: "/desempeno-social", hasSubmenu: true },
+    {
+      name: "PUBLICACIONES Y NOVEDADES",
+      link: "/publicaciones-y-novedades",
+      hasSubmenu: true,
+    },
+    { name: "PREVENCIÓN", link: "/prevencion", hasSubmenu: true },
+    { name: "CRECEMOS POR TI", link: "/crecemos-por-ti" },
+  ];
 
-    // Agregar el evento de clic al documento
-    document.addEventListener("mousedown", handleClickOutside);
-
-    // Limpiar el evento al desmontar el componente
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef]);
-
-  const [isOpens, setIsOpens] = useState(false); // Manejar el estado del menú móvil
-  const [activeDropdowns, setActiveDropdowns] = useState(null); // Manejar los submenús
-
-  const toggleDropdowns = (key) => {
-    if (activeDropdowns === key) {
-      setActiveDropdowns(null); // Cierra el submenú si ya está abierto
-    } else {
-      setActiveDropdowns(key); // Abre el nuevo submenú
-    }
-  };
   return (
-    <nav className="bg-white ">
-      <div className="max-w-full px-4 mx-auto">
-        <div className="flex items-center justify-between h-16">
-          {/* Logo */}
-          <Link to="/">
-            <div className="flex items-center">
-              <img src={Logo} alt="Cooperativa Logo" className="h-16" />
+    <nav className="w-full bg-white shadow-sm">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-20 lg:h-20">
+          <div className="flex items-center flex-shrink-0">
+            <img className="w-auto h-12 lg:h-16" src={Logo} alt="Logo" />
+          </div>
+          <div className="items-center justify-center flex-1 hidden lg:flex">
+            <div className="flex space-x-4">
+              {topMenuItems.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.link}
+                  className="px-3 py-2 text-sm font-bold text-[#f8961e] rounded-md hover:bg-orange-100 "
+                >
+                  {item.name}
+                </Link>
+              ))}
             </div>
-          </Link>
-
-          {/* Centered Links */}
-          <div
-            className="hidden font-semibold text-black space-x-9 lg:flex"
-            ref={dropdownRef}
-          >
-            <Link to="/" className="hover:text-[#09bc8a]">
-              Inicio
-            </Link>
-
-            {/* Dropdowns */}
-            <div className="relative">
-              <button
-                onClick={() => toggleDropdown("nosotros")}
-                className="flex items-center space-x-1 hover:text-[#09bc8a] "
-              >
-                Nosotros <ChevronDown size={17} color="#09bc8a" />
-              </button>
-              {activeDropdown === "nosotros" && (
-                <div className="absolute w-48 mt-2 bg-white border rounded-lg shadow-lg">
-                  <Link
-                    to="/perfil-institucional"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Perfil Institucional
-                  </Link>
-                  <Link
-                    to="/historia"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Historia
-                  </Link>
-                </div>
-              )}
-            </div>
-
-            <div className="relative">
-              <button
-                onClick={() => toggleDropdown("servicios")}
-                className="flex items-center space-x-1 hover:text-[#09bc8a]"
-              >
-                Servicios <ChevronDown size={17} color="#09bc8a" />
-              </button>
-              {activeDropdown === "servicios" && (
-                <div className="absolute w-48 mt-2 bg-white border rounded-lg shadow-lg">
-                  <Link
-                    to="/educacion"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Educación
-                  </Link>
-                  <Link
-                    to="/telecomunicaciones"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Telecomunicaciones
-                  </Link>
-                  <Link
-                    to="/financiamiento"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Financiamiento
-                  </Link>
-                </div>
-              )}
-            </div>
-
-            <div className="relative">
-              <button
-                onClick={() => toggleDropdown("socios")}
-                className="flex items-center space-x-1 hover:text-[#09bc8a] "
-              >
-                Socios <ChevronDown size={17} color="#09bc8a" />
-              </button>
-              {activeDropdown === "socios" && (
-                <div className="absolute w-48 mt-2 bg-white border rounded-lg shadow-lg">
-                  <Link
-                    to="/socio-individual"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Socio Individual
-                  </Link>
-                  <Link
-                    to="/socio-empresa"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Socio Empresa
-                  </Link>
-                </div>
-              )}
-            </div>
-
-            <Link to="/prensa" className="hover:text-[#09bc8a]">
-              Prensa
-            </Link>
-            <Link to="/sucursales" className="hover:text-[#09bc8a]">
-              Sucursales
-            </Link>
+          </div>
+          <div className="items-center hidden lg:flex">
+            <button className="flex items-center px-4 py-2 text-sm font-medium text-white bg-green-500 rounded-md hover:bg-green-600">
+              Buscar
+              <Search className="w-4 h-4 ml-2" />
+            </button>
           </div>
 
-          {/* Call-to-action Button */}
-          <div className="items-center hidden space-x-4 lg:flex">
-            <button className="text-[#09bc8a] ">
-              <Search size={28} />
-            </button>
-            <Link
-              to="/contactos"
-              className="text-white  px-4 py-2 rounded-full font-semibold bg-[#09bc8a] hover:text-white transition-all duration-300"
+          <div className="flex items-center lg:hidden">
+            <button
+              onClick={toggleMenu}
+              className="inline-flex items-center justify-center p-2 text-green-400 rounded-md bg-green-50 "
             >
-              Contáctanos
-            </Link>
-          </div>
-
-          {/* Mobile Search and Menu Icons */}
-          <div className="flex items-center space-x-3 lg:hidden">
-            <button onClick={toggleMenu} className="text-[#09bc8a]">
-              {isOpen ? <X size={28} /> : <Menu size={28} />}
-            </button>
-            <button className="text-[#09bc8a]">
-              <Search size={28} />
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <X className="block w-6 h-6" aria-hidden="true" />
+              ) : (
+                <Menu className="block w-6 h-6" aria-hidden="true" />
+              )}
             </button>
           </div>
-        </div>
-
-        {/* Mobile Menu */}
-        <div
-          className={`border rounded-lg lg:hidden fixed top-0 right-0 w-3/4 h-full bg-white text-[#f8961e] transform transition-transform duration-300 ${
-            isOpen ? "translate-x-0" : "translate-x-full"
-          }`}
-        >
-          <div className="flex justify-end p-4">
-            <button onClick={toggleMenu}>
-              <X size={28} />
-            </button>
-          </div>
-          <ul className="flex flex-col items-start flex-grow w-full p-4 space-y-2 bg-white">
-            <li>
-              <Link
-                to="/"
-                className=" hover:text-[#f8961e] transition-colors flex justify-between items-center w-full text-[#09bc8a] font-bold px-4 py-2"
-              >
-                Inicio
-              </Link>
-            </li>
-            <li className="relative w-full">
-              <button
-                onClick={() => toggleDropdowns("nosotros")}
-                className="flex justify-between items-center w-full text-[#09bc8a] font-bold px-4 py-2 hover:bg-gray-100 rounded transition-colors"
-              >
-                Nosotros <ChevronDown size={17} />
-              </button>
-              {activeDropdowns === "nosotros" && (
-                <div className="absolute left-0 z-50 w-full mt-2 bg-white border rounded-lg shadow-lg">
-                  <Link
-                    to="/perfil-institucional"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Perfil Institucional
-                  </Link>
-                  <Link
-                    to="/historia"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Historia
-                  </Link>
-                </div>
-              )}
-            </li>
-
-            <li className="relative w-full">
-              <button
-                onClick={() => toggleDropdowns("servicios")}
-                className="flex justify-between items-center w-full text-[#09bc8a] font-bold px-4 py-2 hover:bg-gray-100 rounded transition-colors"
-              >
-                Servicios <ChevronDown size={17} />
-              </button>
-              {activeDropdowns === "servicios" && (
-                <div className="absolute left-0 z-50 w-full mt-2 bg-white border rounded-lg shadow-lg">
-                  <Link
-                    to="/servicios/servicio1"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Educación
-                  </Link>
-                  <Link
-                    to="/servicios/servicio2"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Telecomunicaciones
-                  </Link>
-                  <Link
-                    to="/servicios/servicio1"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Financiamiento
-                  </Link>
-                </div>
-              )}
-            </li>
-
-            <li className="relative w-full">
-              <button
-                onClick={() => toggleDropdowns("socios")}
-                className="flex justify-between items-center w-full text-[#09bc8a] font-bold px-4 py-2 hover:bg-gray-100 rounded transition-colors"
-              >
-                Socios <ChevronDown size={17} />
-              </button>
-              {activeDropdowns === "socios" && (
-                <div className="absolute left-0 z-50 w-full mt-2 bg-white border rounded-lg shadow-lg">
-                  <Link
-                    to="/socios-individual"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Socio Individual
-                  </Link>
-                  <Link
-                    to="/socios-empresa"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Socio Empresa
-                  </Link>
-                </div>
-              )}
-            </li>
-
-            <li className="relative w-full">
-              <button className="flex justify-between items-center w-full text-[#09bc8a] font-bold px-4 py-2 hover:bg-gray-100 rounded transition-colors">
-                <Link to="/">Prensa </Link>
-              </button>
-            </li>
-
-            <li className="relative w-full">
-              <button className="flex justify-between items-center w-full text-[#09bc8a] font-bold px-4 py-2 hover:bg-gray-100 rounded transition-colors">
-                <Link to="/">Sucursales </Link>
-              </button>
-            </li>
-
-            <li className="relative w-full">
-              <Link
-                to="/contactos"
-                className="block w-full bg-[#09bc8a] text-white px-4 py-2 rounded-full font-bold text-center hover:bg-[#f8961e] transition-colors"
-              >
-                Contáctanos
-              </Link>
-            </li>
-          </ul>
         </div>
       </div>
+      <div className="hidden bg-green-500 lg:block">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex space-x-8">
+            {bottomMenuItems.map((item) => (
+              <div key={item.name} className="relative group">
+                <Link
+                  to={item.link}
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-green-600"
+                >
+                  {item.name}
+                  {item.hasSubmenu && <ChevronDown className="w-4 h-4 ml-1" />}
+                </Link>
+                {item.hasSubmenu && (
+                  <div className="absolute left-0 invisible w-48 mt-2 transition-all duration-300 ease-in-out bg-white rounded-md shadow-lg opacity-0 ring-1 ring-black ring-opacity-5 group-hover:opacity-100 group-hover:visible">
+                    <div className="py-1">
+                      <Link
+                        to="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Submenu Item 1
+                      </Link>
+                      <Link
+                        to="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Submenu Item 2
+                      </Link>
+                      <Link
+                        to="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      >
+                        Submenu Item 3
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="lg:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {topMenuItems.map((item) => (
+              <Link
+                key={item.name}
+                to={item.link}
+                className="block px-3 py-2 text-base font-medium text-orange-400 rounded-md hover:bg-orange-50"
+                onClick={toggleMenu}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="px-2 pt-2 pb-3 space-y-1 bg-green-500">
+            {bottomMenuItems.map((item) => (
+              <div key={item.name}>
+                <button
+                  onClick={() => toggleDropdown(item.name)}
+                  className="inline-flex items-center justify-between w-full px-3 py-2 text-base font-medium text-left text-white rounded-md hover:bg-green-600"
+                >
+                  {item.name}
+                  {item.hasSubmenu && <ChevronDown className="w-4 h-4 ml-1" />}
+                </button>
+                {item.hasSubmenu && activeDropdown === item.name && (
+                  <div className="pl-4 space-y-1">
+                    <Link
+                      to="#"
+                      className="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-green-600"
+                    >
+                      Submenu Item 1
+                    </Link>
+                    <Link
+                      to="#"
+                      className="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-green-600"
+                    >
+                      Submenu Item 2
+                    </Link>
+                    <Link
+                      to="#"
+                      className="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-green-600"
+                    >
+                      Submenu Item 3
+                    </Link>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <div className="px-4 py-3">
+            <button className="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white transition-all duration-300 ease-in-out bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2">
+              Buscar
+              <Search className="w-5 h-5 ml-2" />
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
